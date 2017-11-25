31a9ac392839a0f1a3dcbe6c7ffa07c4070e6ecb
Andrew Olden <Andrew.Olden@outlook.com>
2017-11-25 09:32:05 +0000

    Implemented 20 test cases - 10 each way between radians - degreees and vice versa. They have to be rounded due to the use of floats and minor differences at the tail end

--- OSGB36/OSGB36Tests/Coordinae_Support_Tests.cs
+++ OSGB36/OSGB36Tests/Coordinae_Support_Tests.cs
@@ -13,14 +13,38 @@ namespace OSGB36Tests
     public class Coordinae_Support_Tests
     {
         [TestCase(0.5235987756,30)]
+        [TestCase(0.7853981634,45)]
+        [TestCase(1.0471975512, 60)]
+        [TestCase(1.5707963268, 90)]
+        [TestCase(2.0943951024, 120)]
+        [TestCase(2.3561944902, 135)]
+        [TestCase(2.6179938780, 150)]
+        [TestCase(3.1415926536, 180)]
+        [TestCase(4.7123889804, 270)]
+        [TestCase(6.2831853072, 360)]
         public void Test_Conversion_Of_Radians_To_Degrees(double a,double b)
         {
             ICoodrinateSupport C = new CoordinateSupport();
-            var d = Math.Round(C.RadiansToDegrees(a),5);
+            var d = Math.Round(C.RadiansToDegrees(a),1);
 
             Assert.AreEqual(b, d);
+        }
 
-
+        [TestCase(0.5235987756, 30)]
+        [TestCase(0.7853981634, 45)]
+        [TestCase(1.0471975512, 60)]
+        [TestCase(1.5707963268, 90)]
+        [TestCase(2.0943951024, 120)]
+        [TestCase(2.3561944902, 135)]
+        [TestCase(2.6179938780, 150)]
+        [TestCase(3.1415926536, 180)]
+        [TestCase(4.7123889804, 270)]
+        [TestCase(6.2831853072, 360)]
+        public void Test_Conversion_Of_Degrees_to_Radians(double a, double b)
+        {
+            ICoodrinateSupport C = new CoordinateSupport();
+            var d = Math.Round(C.DegreeesToRadians(b),10);
+            Assert.AreEqual(a, d);
         }
     }
 }
