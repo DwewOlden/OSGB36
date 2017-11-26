09c9141323219bbc5d196272a9f1d0e6ef50902a
Andrew Olden <Andrew.Olden@outlook.com>
2017-11-26 16:17:19 +0000

    Added a latitude and longtitude class and test. This is needed because of the exta resolution needed in decimal degree (five decimal places rather than three in eOSGB36)

---
+++ OSGB36/OSGB36/Coordinate/LatLon.cs
@@ -0,0 +1,107 @@
+﻿using OSGB36.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSGB36.Coordinate
+{
+    public class LatLon:ICoordinate 
+    {
+        /// <summary>
+        /// The latitude of the coordinate
+        /// </summary>
+        protected readonly double _latitude;
+
+        /// <summary>
+        /// The longtitude of the coordinate.
+        /// </summary>
+        protected readonly double _longtitude;
+
+        /// <summary>
+        /// The distance above see level of the location.
+        /// </summary>
+        protected readonly double _height;
+
+        /// <summary>
+        /// The distance in meters east of the reference point.
+        /// </summary>
+        /// <remarks>
+        /// Longtitude goes across - see easting
+        /// </remarks>
+        public double Longtitude
+        {
+            get
+            {
+                return _longtitude;
+            }
+        }
+
+        /// <summary>
+        /// The distance in meters north of the the reference point.
+        /// </summary>
+        /// <remarks>
+        /// Latitude goes up - see  northing
+        /// </remarks>
+        public double Latitude
+        {
+            get
+            {
+                return _latitude;
+            }
+        }
+
+        /// <summary>
+        /// The distance above see level (reference point Newlyn).
+        /// </summary>
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="pLatitude">The latitude of the location</param>
+        /// <param name="pLongtitude">The longtitude of the location</param>
+        /// <param name="pHeight">The height above sea level of the location</param>
+        public LatLon(double pLatitude, double pLongtitude, double pHeight)
+        {
+            // Round the the locations down to 5 decimal places - that will be mm level
+            // as the major component are meters. Thats why its in a different class.
+
+            _latitude = Math.Round(pLatitude, 5);
+            _longtitude = Math.Round(pLongtitude, 5);
+            _height = Math.Round(pHeight);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Latitude:{0} Longtitude:{1} Height:{2}", _latitude, _longtitude, _height);
+        }
+
+        public override int GetHashCode()
+        {
+            return (((int)_latitude + (int)_longtitude) + ((int)_height * 3));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this.GetType() != obj.GetType())
+                return false;
+            else
+            {
+                LatLon e = (LatLon)obj;
+                if ((Math.Round(e._latitude, 5) == Math.Round(_latitude, 5)) && (Math.Round(e._longtitude, 5) == Math.Round(_longtitude, 5)) &&
+                    (Math.Round(e._height, 5) == Math.Round(_height, 5)))
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+}
---
+++ OSGB36/OSGB36Tests/Lat_Lon_Tests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OSGB36.Coordinate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSGB36Tests
+{
+    [TestFixture]
+    public class Lat_Lon_Tests
+    {
+        [TestCase(10, 20, 30, true)]
+        [TestCase(20, 50, 70, true)]
+        [TestCase(20, 230, 80, true)]
+        public void EqualityTest(double e, double n, double h, bool e1)
+        {
+            LatLon k = new LatLon(e, n, h);
+            LatLon k2 = new LatLon(e, n, h);
+
+            Assert.AreEqual(k, k2);
+        }
+
+        [TestCase(10, 20, 30, 11, 20, 30, true)]
+        [TestCase(20, 50, 70, 20, 51, 70, true)]
+        [TestCase(20, 230, 80, 20, 230, 81, true)]
+        [TestCase(20, 230, 80, 1, 1, 1, true)]
+        public void EqualityTest_2(double e, double n, double h, double e2, double n2, double h2, bool e1)
+        {
+            LatLon k = new LatLon(e, n, h);
+            LatLon k2 = new LatLon(e2, n2, h2);
+
+            Assert.AreNotEqual(k, k2);
+        }
+
+        [TestCase(10.123446, 20, 30, 10.12345, 20, 30, true)]
+        public void EqualityTest_3(double e, double n, double h, double e2, double n2, double h2, bool e1)
+        {
+            LatLon k = new LatLon(e, n, h);
+            LatLon k2 = new LatLon(e2, n2, h2);
+
+            Assert.AreEqual(k, k2);
+        }
+
+        [Test]
+        public void ToString_Test()
+        {
+            LatLon k = new LatLon(10, 20, 30);
+            string outcome = k.ToString();
+            string expected = "Latitude:10 Longtitude:20 Height:30";
+
+            Assert.AreEqual(expected, outcome);
+
+        }
+    }
+}
